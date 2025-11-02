37a65baa7338ef40c0ba427d9728e687a5051383
sayakr428 <92079116+sayakr428@users.noreply.github.com>
2025-11-03 01:41:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,12 +66,4 @@ resource "aws_s3_bucket_ownership_controls" "this" {
   }
 }
 
-# -------- Summary output --------
-output "summary" {
-  value = {
-    ec2_count = length(aws_instance.ec2)
-    s3_names  = [for _, b in aws_s3_bucket.buckets : b.bucket]
-    project   = var.project
-    region    = var.aws_region
-  }
-}
+
