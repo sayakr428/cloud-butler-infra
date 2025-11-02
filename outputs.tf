46001b0a86d94ad14e5b30029e96f41e10cbeb64
sayakr428 <92079116+sayakr428@users.noreply.github.com>
2025-11-03 01:41:58 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "summary" {
   value = {
-    region    = var.aws_region
+    ec2_count = length(aws_instance.ec2)
+    s3_names  = [for _, b in aws_s3_bucket.buckets : b.bucket]
     project   = var.project
-    ec2_count = local.ec2_count
-    s3_names  = keys(aws_s3_bucket.buckets)
+    region    = var.aws_region
   }
 }
